ccf1e8728a7f1afd180e731b9a97f6cbfe886838
William Mian <william_mian@yahoo.com.br>
2024-08-23 16:39:28 -0300

    consultas

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -21,6 +21,7 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
     @Query("select s from Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
     List<Serie> seriesPorTemporadaEAValiacao(int totalTemporadas, double avaliacao);
+
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
     List<Episodio> episodiosPorTrecho(String trechoEpisodio);
 
